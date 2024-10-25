daa3595115fe816b4a693934b8dffda77107abe8
Oscar Medina Paz <oscar.medina01@unicatolica.edu.co>
2024-10-24 23:46:48 -0500

    correcion de vista en presentacion de los datos

--- GanaLoco/src/components/UserHome.jsx
+++ GanaLoco/src/components/UserHome.jsx
@@ -50,7 +50,7 @@ function UserHome() {
           {codes.map((code) => (
             <tr key={code._id}>
               <td>{code.code}</td>
-              <td>{new Date(code.date).toLocaleDateString()}</td>
+              <td>{code.date}</td>
               <td>{code.Ganador ? 'Sí' : 'No'}</td>
             </tr>
           ))}
