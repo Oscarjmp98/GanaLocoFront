edd105e8f42bd005799815445031719d6a18b03e
Oscar Medina Paz <oscar.medina01@unicatolica.edu.co>
2024-10-24 23:59:29 -0500

    Faltaba el .css

--- GanaLoco/src/components/UserHome.jsx
+++ GanaLoco/src/components/UserHome.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './styles/UserHome';
+import './styles/UserHome.css';
 
 function UserHome() {
   const [code, setCode] = useState('');
